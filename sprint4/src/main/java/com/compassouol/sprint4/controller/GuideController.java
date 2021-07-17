c199f76ef0a2b3d97aefaabcfa530de100d76af0
domafras <85114157+domafras@users.noreply.github.com>
2021-07-17 19:07:21 -0300

    Incremental commit
    Swagger Springfox

--- sprint4/src/main/java/com/compassouol/sprint4/controller/GuideController.java
+++ sprint4/src/main/java/com/compassouol/sprint4/controller/GuideController.java
@@ -4,17 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 @Controller
+@ApiIgnore
 public class GuideController {
 
 	@RequestMapping("/")
 	@ResponseBody
 	public String guide() {
-		return "Endpoints: "
-				+ "/auth"
-				+ "/actuator"
-				+ "/pessoa "
-				+ "/protected/produto "
-				+ "/protected/pedido";
+		return "Endpoints:"
+				+ " /auth,"
+				+ " /actuator/**,"
+				+ " /pessoa/**,"
+				+ " /protected/produto/**,"
+				+ " /protected/pedido/**;";
 	}
 }
